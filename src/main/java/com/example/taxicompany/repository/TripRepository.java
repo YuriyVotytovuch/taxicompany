58dfe8156ce2ca2dafbb5591bbc0168f8db12a53
ShmyhelskyiAndriy <126695567+ShmyhelskyiAndriy@users.noreply.github.com>
2025-05-20 22:44:49 +0300

    update 17

--- src/main/java/com/example/taxicompany/repository/TripRepository.java
+++ src/main/java/com/example/taxicompany/repository/TripRepository.java
@@ -1,11 +1,9 @@
 package com.example.taxicompany.repository;
 
 import com.example.taxicompany.entity.Trip;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
--- src/main/java/com/example/taxicompany/security/SecurityConfig.java
+++ src/main/java/com/example/taxicompany/security/SecurityConfig.java
@@ -1,12 +1,9 @@
 package com.example.taxicompany.security;
 
-import com.example.taxicompany.entity.User;
 import com.example.taxicompany.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
